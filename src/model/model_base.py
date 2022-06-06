1302a200d79de79c6049bb2d31fb5dcafd9580c6
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-06 09:11:22 +0200

    ajout de la methode pour recuperez les données dans le fichier | ajout des docstring

--- src/model/model_base.py
+++ src/model/model_base.py
@@ -1,9 +1,23 @@
-import json 
+import json
 import constant
 
-from pathlib import Path 
+from pathlib import Path
 
-class ModelBase: 
+
+class ModelBase:
+    """Classe model de base, cette classe sera heriter des enfants model."""
+    
     def __init__(self):
         self.const = constant
+
+    def get_content_data(self):
+        """Donne les données sauvegarder dans le fichiers des liens 
+
+        Returns:
+            dict: contenue du fichier 
+        """
         
+        path = self.const.FOLDER_DATA + self.const.FILE_LINK_SAVING
+        if Path(path).exists():
+            with open(path) as f:
+                return json.loads(path)
