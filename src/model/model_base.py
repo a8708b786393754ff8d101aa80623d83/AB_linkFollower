f78911ea25262fe6704b08f7dbc408313932fd26
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-06 12:38:33 +0200

    ajout attribut

--- src/model/model_base.py
+++ src/model/model_base.py
@@ -6,17 +6,18 @@ from pathlib import Path
 
 class ModelBase:
     """Classe model de base, cette classe sera heriter des enfants model."""
-    
+
     def __init__(self):
         self.const = const
+        self.path_link_file = self.const.FOLDER_DATA + self.const.FILE_LINK_SAVING
 
     def get_content_data(self):
         """Donne les données sauvegarder dans le fichiers des liens 
         Returns:
             dict: contenue du fichier 
         """
-        
-        path = self.const.FOLDER_DATA + self.const.FILE_LINK_SAVING
-        if Path(path).exists():
-            with open(path) as f:
-                return json.loads(path)
+
+        if Path(self.path_link_file).exists():
+            with open(self.path_link_file) as f:
+                return json.loads(self.path_link_file)
+
