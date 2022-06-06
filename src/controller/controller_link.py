82d144fa47dca0ebf26972e916295928e6f3a829
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-06 14:50:10 +0200

    ajout de la methode qui recupere tout les liens de la page

--- src/controller/controller_link.py
+++ src/controller/controller_link.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 from bs4 import BeautifulSoup
 
 from .controller_base import ControllerBase
@@ -7,26 +7,35 @@ from .controller_base import ControllerBase
 class ControllerLink(ControllerBase):
     def __init__(self, model, view):
         super().__init__(model, view)
-        
-    
+
     def get_links_css(self, url: str):
         resp = self.requests_link(url)
-        if not resp is None: 
-            return  BeautifulSoup(resp.text, 'lxml').find_all('link', {'rel': 'stylesheet'})
-            
-        
-    def get_links_js(self, url: str): 
+        if not resp is None:
+            return BeautifulSoup(resp.text, 'lxml').find_all('link', {'rel': 'stylesheet'})
+
+    def get_links_js(self, url: str):
         resp = self.requests_link(url)
-        if not resp is None: 
+        if not resp is None:
             return BeautifulSoup(resp.text, 'lxml').find_all('script')
-        
-    def get_links_img(self, url: str): 
+
+    def get_links_img(self, url: str):
         resp = self.requests_link(url)
-        if not resp is None: 
+        if not resp is None:
             return BeautifulSoup(resp.text, 'lxml').find_all('img')
-        
-    def get_links_tag_a(self, url: str): 
+
+    def get_links_tag_a(self, url: str):
         resp = self.requests_link(url)
-        if not resp is None: 
+        if not resp is None:
             return BeautifulSoup(resp.text, 'lxml').find_all('a')
+
+        """faire une methode qui identifie a qui appartient les liens 
+        """
+
+    def get_links(self, url: str):
+        data = []
+        data.append(self.get_links_css(url))
+        data.append(self.get_links_img(url))
+        data.append(self.get_links_js(url))
+        data.append(self.get_links_tag_a(url))
         
+        return data
