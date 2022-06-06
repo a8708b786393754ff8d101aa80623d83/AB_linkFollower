7f7b83e38eee819f1ede08842df7febad2a552cf
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-06 10:02:29 +0200

    ajout de la methode is_link

--- src/controller/controller_base.py
+++ src/controller/controller_base.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 from bs4 import BeautifulSoup
 
@@ -7,8 +8,23 @@ class ControllerBase:
     def __init__(self, model, view):
         self.model = model()
         self.view = view()
+        self.data = {
+            'url': None
+        }
 
     def requests_link(self, url: str):
         r = requests.get(url)
         if r.ok:
+            self.data['url'] = url  # type: ignore
             return r
+
+    def is_link(self, url: str):
+        """Determine si un element est un lien. 
+        Args: 
+            url(str): lien 
+
+        Returns: 
+            bool: True si c'est un lien. False si ce n'est pas un lien
+        """
+        
+        return re.search("^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$", url)
