37540360625d1529f05f4a58654affc2f2ee38be
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-11 22:29:24 +0200

    supprimer les note

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ from src.controller import controller_link as c
 from src.model import model_link as m
 from src.view import view_link as v
 
-"""
-    FAIRE UN SCRIPT QUI SUIS UN LIEN EST QUI SUIS LES LIEN QUI EST DANS LE LIEN
-    TOUT CA C'EST RECURSIF, EN MVC
-    LES LIENS SONT ENREGISTREZ DANS UN FICHIER EN JSON
-    LES CLEF C'EST LE SITE QUI CONTIENT LE LIEN EST SONT LIEN 
-    
-    1: lui donner un lien
-    2: il liste tout les liens qui sont dans la page 
-    3: il identifie les liens qui appartient a cette page
-    4: il parcour les liens est ainsi de suite 
-"""
 url = 'https://ecurie-des-4-chemins.fr/'
 
 controller = c.ControllerLink(m.ModelLink, v.ViewBase)
