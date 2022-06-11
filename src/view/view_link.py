1544608fec3b8fb7e010d000caf9c77a3f662867
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-11 22:20:33 +0200

    ajout attribut compteur

--- src/view/view_link.py
+++ src/view/view_link.py
@@ -4,18 +4,28 @@ from .view_base import ViewBase
 class ViewLink(ViewBase):
     def __init__(self):
         super().__init__()
+        self.internal = 0 
+        self.external = 0 
+        self.script = 0 
+        self.tel = 0 
+        self.mail = 0 
 
     def link_internal(self, link):
+        self.internal += 1 
         print(self.fore.RED, link, self.fore.RESET)
 
     def link_external(self, link):
+        self.external += 1
         print(self.fore.BLUE, link, self.fore.RESET)
 
     def script_javascript(self, script):
+        self.script += 1 
         print(self.fore.YELLOW, script, self.fore.RESET)
 
     def link_tel(self, link_base): 
+        self.tel += 1 
         print(self.fore.GREEN, link_base, self.fore.RESET)
         
     def link_mail(self, link): 
+        self.mail += 1 
         print(self.fore.CYAN, link, self.fore.RESET)
