1bf16bdf95b823ab7d42769ef3e5d4937b6c6bfc
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-12 22:44:42 +0200

    ajout docstring model base

--- src/model/model_base.py
+++ src/model/model_base.py
@@ -5,15 +5,22 @@ from pathlib import Path
 
 
 class ModelBase:
-    """Classe model de base, cette classe sera heriter des enfants model."""
+    """Classe de base poour les models.
+
+    Attributes:
+        data (module): module qui contient toute les constantes.
+    """
 
     def __init__(self):
+        """Méthode __init__, initialise l'attribut const au module."""
+
         self.const = const
 
     def get_content_data(self, path: str):
-        """Donne les données sauvegarder dans le fichier données 
+        """Donne les données sauvegarder dans le fichier données.
+         
         Returns:
-            dict: contenue du fichier 
+            dict: contenue du fichier.
         """
 
         if Path(path).exists():
