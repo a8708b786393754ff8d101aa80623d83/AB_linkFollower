738bd3bbd0dfb5959e0dde2318a4f1492357fc70
ayoub68000 <11275224-a6888b@users.noreply.gitlab.com>
2022-06-12 22:47:18 +0200

    ajout docstring view base

--- src/view/view_base.py
+++ src/view/view_base.py
@@ -2,7 +2,15 @@ from colorama import Fore, Back
 
 
 class ViewBase:
+    """Classe de base poour les vue.
+
+    Attributes:
+        fore (obj): objet qui change les couleur de texte
+        back (obj): objet qui change les couleur du fond
+    """
+
     def __init__(self):
+        """Méthode __init__, initialise les attribut au classe."""
+
         self.fore = Fore
-        self.back = Back 
-        
+        self.back = Back
