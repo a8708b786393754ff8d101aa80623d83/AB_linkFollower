b6cea067888e402ec34648ef170d94418560b975
a6888b <ayoubbrahim68@gmail.com>
2023-01-13 14:04:48 +0100

    regalge import and add args

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 #! /usr/bin/python3
-from src.controller import controller_link as c
-from src.model import model_link as m
-from src.view import view_link as v
-
+import args
 import constant as const
+from src.controller.controller_link import ControllerLink
+from src.model.model_link import ModelLink
+from src.view.view_base import ViewBase
+from src.view.view_link import ViewLink
 
-url = 'https://www.youtube.com/watch?v=0D7WSDugT6g'
+arg = args.arguments()
 
-controller = c.ControllerLink(m.ModelLink, v.ViewBase)
+
+controller = ControllerLink(ModelLink, ViewBase)
 controller.model.set_path_link(const.FOLDER_DATA + const.FILE_LINK_SAVING)
 
-view = v.ViewLink()
+view = ViewLink()
 
-if not controller.requests_link(url) is None:
-    for keys, links in controller.get_links(url).items():
+if not controller.requests_link(arg.url) is None:
+    for keys, links in controller.get_links(arg.url).items():
         for link in links:  # type: ignore
             if controller.model.is_script(link, const.ARRAY_TYPE_SCRIPT):
                 view.script_javascript(link)
