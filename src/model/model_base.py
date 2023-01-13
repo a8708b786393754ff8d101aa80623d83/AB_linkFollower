9695ca578e66795b9253d6016396c8e7300b382b
a6888b <ayoubbrahim68@gmail.com>
2023-01-13 13:24:50 +0100

    delete method construct

--- src/model/model_base.py
+++ src/model/model_base.py
@@ -1,6 +1,4 @@
 import json
-import src.model.constant as const
-
 from pathlib import Path
 
 
@@ -10,12 +8,7 @@ class ModelBase:
     Attributes:
         data (module): module qui contient toute les constantes.
     """
-
-    def __init__(self):
-        """Méthode __init__, initialise l'attribut const au module."""
-
-        self.const = const
-
+    
     def get_content_data(self, path: str):
         """Donne les données sauvegarder dans le fichier données.
          
