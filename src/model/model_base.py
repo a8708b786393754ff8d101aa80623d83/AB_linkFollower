ff36f4c2fe209407fc60730f1e00047d4f1a4473
a6888b <ayoubbrahim68@gmail.com>
2023-01-13 14:25:19 +0100

    add method get_url_base

--- src/model/model_base.py
+++ src/model/model_base.py
@@ -1,3 +1,4 @@
+import re
 import json
 from pathlib import Path
 
@@ -8,6 +9,29 @@ class ModelBase:
     Attributes:
         data (module): module qui contient toute les constantes.
     """
+    def get_url_base(self, link, class_html: list):
+        """Méthode qui fait utilise les regex pour voir si on peut recuperer le lien de base. 
+
+        Args:
+            link (str| bs4.Element.Tag): lien
+
+        Returns:
+            str: lien 
+        """
+
+        try:
+            pattern = re.compile(str(link))
+        except:
+            pass  # trouver quelle erruer extactement
+        else:
+            for prefix in class_html:
+                # regarde si le pattern(link) contient le prefix
+                if not re.match(pattern, prefix):
+                    if not link.get(prefix) is None:
+                        return link.get(prefix)
+        finally:
+            if link.name == 'a':
+                return link.get('href')
     
     def get_content_data(self, path: str):
         """Donne les données sauvegarder dans le fichier données.
